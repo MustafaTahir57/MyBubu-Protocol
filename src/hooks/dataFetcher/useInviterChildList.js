f02994abb5e93498aecd10991cdcd85b1cc3f2ae
MustafaTahir57 <mustafatahir802@gmail.com>
2026-03-10 16:32:25 +0500

    Addresses adjusted

--- src/hooks/dataFetcher/useInviterChildList.js
+++ src/hooks/dataFetcher/useInviterChildList.js
@@ -1,16 +1,16 @@
 import { useReadContract } from 'wagmi';
-import { CONTRACT_ADDRESSES, MYBOO_PRESALE_ABI, ACTIVE_CHAIN_ID } from '@/config/contracts';
+import { CONTRACT_ADDRESSES, MYBUBU_ABI, ACTIVE_CHAIN_ID } from '@/config/contracts';
 
 export const useInviterChildList = (address) => {
   const { data, isLoading, refetch } = useReadContract({
-    address: CONTRACT_ADDRESSES[ACTIVE_CHAIN_ID].MYBOO_PRESALE,
-    abi: MYBOO_PRESALE_ABI,
+    address: CONTRACT_ADDRESSES[ACTIVE_CHAIN_ID].MYBUBU_TOKEN,
+    abi: MYBUBU_ABI,
     functionName: 'getInviterChildList',
     args: address ? [address] : undefined,
     query: { enabled: !!address },
   });
 
-  const referralCount = data ? data.length : 0;
+  const referralCount = data ? data?.length : 0;
 
   return { referralCount, isLoading, refetch };
 };
